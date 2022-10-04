d6f53e9a1bb87dd15d8f487ffd5ca89cee915866
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-10-04 11:12:15 -0400

    creater product router, add app.use route to server file

---
+++ routes/productRouter.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+
+
+
+router.route('/products')
+    .get(productCtrl.getProducts)
+    .post(auth, authAdmin, productCtrl.createProduct)
+
+
+router.route('/products/:id')
+    .delete(auth, authAdmin, productCtrl.deleteProduct)
+    .put(auth, authAdmin, productCtrl.updateProduct)
+
+
+
+module.exports = router
