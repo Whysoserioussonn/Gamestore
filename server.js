9d7fe59e406dabe2b64eb4385e312e2775bfb250
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-10-04 10:17:29 -0400

    update upload route to include size limts , formats jpg png and webp

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(fileUpload({
 // Routes
 app.use('/user', require('./routes/userRouter'))
 app.use('/api', require('./routes/categoryRouter'))
+app.use('/api', require('./routes/upload'))
+
 
 
 
