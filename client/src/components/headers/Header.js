0a9175ab1d5f12f9f029c80d160ab50a58ac562e
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-10-04 19:10:34 -0400

    style header.css for top tab of webpage with icons

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -3,12 +3,41 @@ import {GlobalState} from '../../GlobalState'
 import Menu from './icon/menu.svg'
 import Close from './icon/close.svg'
 import Cart from './icon/cart.svg'
+import {Link} from 'react-router-dom'
+
 
 function Header() {
     const value = useContext(GlobalState)
     return (
         <header>
-           
+            <div className="menu">
+                <img src={Menu} alt="" width="30" />
+            </div>
+
+            <div className="logo">
+                <h1>
+                    <Link to="/">HenRY's GaMEsToRe</Link>
+                </h1>
+            </div>
+
+            <ul>
+            <li><Link to="/">Products</Link></li>
+            <li><Link to="/login">Login ✥ Register</Link></li>
+
+            <li>
+                    <img src={Close} alt="" width="30" />
+                </li>
+
+            </ul>
+
+            <div className="cart-icon">
+                <span>0</span>
+                <Link>
+                <img src={Cart} alt="" width="30" />
+                </Link>
+
+            </div>
+
         </header>
     )
 }
