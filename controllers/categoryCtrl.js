f0d30e9d0b817af617ebf397ffa37288e3987490
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-10-03 22:30:19 -0400

    create cagegory: router, controller, app.use route @ server

---
+++ controllers/categoryCtrl.js
@@ -0,0 +1,18 @@
+const categoryCtrl = {
+
+}  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = categoryCtrl
---
+++ routes/categoryRouter.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const categoryCtrl = require('../controllers/categoryCtrl')
+
+
+router.route('/category')
+    .get()
+
+
+
+
+module.exports = router    
